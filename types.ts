76722dc52a55347fb9481d72bd20edf2b330ca91
Justin <justinjoyn@gmail.com>
2025-03-16 20:42:55 +0000

    chore: add types

---
+++ types.ts
@@ -0,0 +1,12 @@
+export type Plant = {
+  name: string;
+  species: string;
+  link: string;
+};
+
+export type PlantDetail = {
+  species: string;
+  link: string;
+  introduction: string;
+  description: string;
+};
